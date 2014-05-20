785c7a7e4359d1e098a703dbae982f7d2b8dc86b
Sean Dooley <sean.dooley.22@gmail.com>
2014-05-20 20:16:43 +0100

    Displaying Product Details

---
+++ BalloonShop/Product.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Product.aspx.cs" Inherits="BalloonShop.Product" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <asp:Label runat="server" ID="titleLabel" CssClass="CatalogTitle" Text="Label" />
+    </p>
+    <p>
+        <asp:Image runat="server" ID="productImage" />
+    </p>
+    <p>
+        <asp:Label runat="server" ID="descriptionLabel" Text="Label" />
+    </p>
+    <p>
+        <b>Price:</b>
+        <asp:Label runat="server" ID="priceLabel" CssClass="ProductPrice" Text="Label" />
+    </p>
+</asp:Content>
---
+++ BalloonShop/Product.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BalloonShop.BusinessTier;
+
+namespace BalloonShop
+{
+    public partial class Product : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Retrieve ProductID from the query string
+            string productId = Request.QueryString["ProductID"];
+
+            // Retrieve product details
+            ProductDetails productDetails = CatalogAccess.GetProductDetails(productId);
+
+            // Does the product exist?
+            if (productDetails.Name != null)
+            {
+                PopulateControls(productDetails);
+            }
+        }
+
+        private void PopulateControls(ProductDetails productDetails)
+        {
+            // Display the product details
+            titleLabel.Text = productDetails.Name;
+            descriptionLabel.Text = productDetails.Description;
+            priceLabel.Text = String.Format("{0:c}", productDetails.Price);
+            productImage.ImageUrl = "ProductImages/" + productDetails.Image;
+
+            // Set the page title
+            this.Title = BalloonShopConfiguration.SiteName + productDetails.Name;
+        }
+    }
+}
